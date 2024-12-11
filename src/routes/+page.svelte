3395acc37cbda49bf99e799fbf2a148f59199aa8
Tim Gu <2013tim.g@gmail.com>
2024-12-11 16:50:40 +0000

    Added logo and updated the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
+	import kiki_logo from "$lib/images/kki_logo.png"
+	
 </script>
 
 <svelte:head>
@@ -9,51 +8,12 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<div class="flex flex-col justify-center items-center p-10 gap-5 max-w-[30rem] m-auto h-screen">
+	<img class="max-h-[15rem] max-w-[15rem]" src={kiki_logo} alt="logo"/>
+	<h1 class="text-2xl font-bold">Welcome to Kiki!</h1>
+	<p class="text-lg">The flashcard app based on Anki</p>
+	<a href={`/auth/login`} class="btn bg-blue-700 max-w-40">Click here to login</a>
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
