ec33eff401a6732a431b633ee4d81f6fb5cd8604
Tim Gu <2013tim.g@gmail.com>
2024-12-10 21:23:11 -0500

    Updated base routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Header from './Header.svelte';
 	import '../app.css';
 
 	let { children } = $props();
@@ -8,33 +7,11 @@
 <div >
 	<!-- <Header /> -->
 
-	<main class="flex flex-col max-w-[40rem] m-auto p-10">
+	<main>
 		{@render children()}
 	</main>
 
-	<!-- <footer>
-		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p>
-	</footer> -->
 </div>
 
 <style>
-	/* .app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	} */
-
-	/* main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	} */
-
 </style>
