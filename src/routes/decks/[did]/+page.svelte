c847a1109177accdd2ea9ddbf1b1ef393f21c287
Tim Gu <2013tim.g@gmail.com>
2024-12-14 23:55:43 -0500

    Fixed Card browsing display.

--- src/routes/decks/[did]/+page.svelte
+++ src/routes/decks/[did]/+page.svelte
@@ -1,13 +1,11 @@
 <!-- src/DeckBrowser.svelte -->  
 <script lang="ts">  
-	// import { getDeck } from '$lib/dummy/adapter';  
 	import {asyncGetDeckCards} from '$lib/api/api';
-	import type { Card } from '$lib/api/types';
+	import type { TypeCard } from '$lib/api/types';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	// import { cards } from '$lib/dummy/dummy';
 
-	let cards : Card[] = $state([{id:0, Front:"", Back:""}]);
+	let cards : TypeCard[] = $state([{cid:0, Front:"", Back:""}]);
 	onMount(async () => {
 		const res = await asyncGetDeckCards(parseInt($page.params.did));
 		console.log(res);
@@ -15,19 +13,42 @@
 	});
 </script>  
 	
-<h2>Cards</h2>  
-<div class="grid grid-row-2 gap-4">
+<!-- <div class="grid grid-row-2 gap-4">
+	<div class="grid grid-cols-3">
+		<div class="col-start-1">
+			Front
+		</div>
+		<div class="col-start-2">
+			Back
+		</div>
+	</div>
 	{#each cards as card}
 		<div class="grid grid-cols-3">
-			<a class="col-start-1" href={`/cards/` + card.id}>
-				{card.id}
-			</a>
-			<div class="col-start2">
+			<div class="col-start1">
 				{card.Front}
 			</div>
-			<div class="col-start-3">
+			<div class="col-start-2">
 				{card.Back}
 			</div>
 		</div>
 	{/each}  
+</div> -->
+<div class="overflow-x-auto">
+	<table class="table table-zebra">
+	  <!-- head -->
+	  <thead>
+		<tr>
+		  <th>Front</th>
+		  <th>Back</th>
+		</tr>
+	  </thead>
+	  <tbody>
+	  {#each cards as card}
+		<tr>
+			<td>{card.Front}</td>
+			<td>{card.Back}</td>
+		</tr>	
+	  {/each}
+	  </tbody>
+	</table>
 </div>
