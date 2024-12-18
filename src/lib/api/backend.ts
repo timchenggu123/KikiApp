0e7ad2ba15eda84cae96e05361b7ac68c97340ae
Tim Gu <2013tim.g@gmail.com>
2024-12-17 21:45:08 -0500

    Removed unnecessary file.

--- src/lib/api/backend.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-const backend = axios.create({
-    baseURL: 'http://127.0.0.1:5000',
-    withCredentials: true, // Automatically include cookies
-  });
-  
-export default backend;
