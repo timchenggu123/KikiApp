4afecd9968d531cbd7e7dbac47090b8f7ccb9d10
Tim Gu <2013tim.g@gmail.com>
2024-12-20 15:18:32 -0500

    Updated cards look

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,6 +2,8 @@
     import {SERVER_URL} from "$lib/api/common";
     let side = $state(true);
     let card_data = $props();
+
+    const base_element = `<base href="${SERVER_URL}/media/" />`;
     // This is used by generated code, so it needs to be a top-level variable
     //compile card front and back as svelte components
     
@@ -9,18 +11,20 @@
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<div onclick={()=>{side=!side}} class="card card-compact gradient-div h-full max-h-[30rem] shadow-xl border-b-2 border-b-zinc-500">
-    <div class={"!card !nightMode card-compact flex flex-col items-center h-full w-full max-h-[30rem] bg-gray-200 backdrop-blur-xl " + (side ? "bg-opacity-80" : "bg-opacity-90")}>
-        <audio src={`${SERVER_URL}/card/${card_data.cid}/back/audio`} class={`p-1 h-[3rem] ` + (side ? `hidden` : ``)} controls></audio>
-        <audio src={`${SERVER_URL}/card/${card_data.cid}/front/audio`} class={`p-1 h-[3rem] ` + (side ? `` : `hidden`)} controls></audio>
-        <div class={"text-xl m-auto p-10 !bg-transparent overflow-hidden " + (side ? `` : `hidden`)}>
-            <!-- {@html card_data ? card_data.Front : `Non-existent`}  -->
-            {@html card_data.Front}
+<div class="rounded-box gradient-div min-h-[40rem] shadow-xl border-b-2 border-b-zinc-500 overflow-y-auto">
+    <div class={"relative rounded-box !nightMode card-compact h-full w-full bg-gray-200 backdrop-blur-xl flex flex-col " + (side ? "bg-opacity-80" : "bg-opacity-90")} onclick={()=>{side=!side}}>
+        <!-- <audio src={`${SERVER_URL}/card/${card_data.cid}/back/audio`} class={`p-1 h-[3rem] ` + (side ? `hidden` : ``)} controls></audio>
+        <audio src={`${SERVER_URL}/card/${card_data.cid}/front/audio`} class={`p-1 h-[3rem] ` + (side ? `` : `hidden`)} controls></audio> -->
+        <div class="flex-grow w-full">
+            <iframe title="card_front" class={"text-xl min-h-full w-full " + (side ? `` : `hidden`)} srcdoc={base_element + card_data.Front}>
+                <!-- {@html card_data ? card_data.Front : `Non-existent`}  -->
+            </iframe>
+            <iframe title="card_back" class={`text-xl min-h-full w-full ` + (side ? `hidden` : ``)} srcdoc={base_element + card_data.Back}>
+                <!-- {@html card_data ? card_data.Back : `Non-existent`}  -->
+                {@html card_data.Back}
+            </iframe>
         </div>
-        <div class={`text-xl m-auto p-10 overflow-hidden ` + (side ? `hidden` : ``)}>
-            <!-- {@html card_data ? card_data.Back : `Non-existent`}  -->
-            {@html card_data.Back}
-        </div>
+        <div class="h-10 w-full text-center text-lg pt-1">Click to flip</div>
     </div>
 </div>
 
