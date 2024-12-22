fce67b6e2f4861eb71c48bbfeb3d6f0f23ad4d1b
Tim Gu <2013tim.g@gmail.com>
2024-12-22 01:19:57 -0500

    Use kikiserver as backend

--- src/lib/api/common.ts
+++ src/lib/api/common.ts
@@ -1,7 +1,7 @@
 import { goto } from '$app/navigation';
 // export const SERVER_URL = "http://192.168.2.10:5000";
-// export const SERVER_URL = "https://kikiserver.timgu.me";
-export const SERVER_URL = "http://localhost:5000";
+export const SERVER_URL = "https://kikiserver.timgu.me";
+// export const SERVER_URL = "http://localhost:5000";
 
 // Check resposne
 export function checkResponse(response: any) {
