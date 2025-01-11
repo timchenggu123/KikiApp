75a60ce1990cfc9bd4c53313dfac95513658d6ce
Tim Gu <2013tim.g@gmail.com>
2025-01-11 00:07:45 -0500

    Unified logo size.

--- src/components/HomeTools.svelte
+++ src/components/HomeTools.svelte
@@ -3,9 +3,9 @@ import kiki_logo from "$lib/images/kiki_logo.png"
 
 </script>
 
-<div class="mt-2 mb-2 grid grid-cols-5 gap-2">
-	<a href="/decks" aria-label="Home" class="flex flex-row">
-	  	<img src={kiki_logo} class="h-15 w-15 p-2"/>
+<div class="flex flex-row gap-2 w-full pt-2">
+	<a href="/decks" aria-label="Home">
+	  	<img src={kiki_logo} class="h-20 w-20 p-2"/>
 	</a>
 </div>
 
